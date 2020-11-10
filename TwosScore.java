ede6d4073a3a48152daffb7ef2f665d8dd8c157a
eZSnake <rehwalde@gmail.com>
2020-11-09 21:32:36 -0800

    Update TwosScore.java

--- TwosScore.java
+++ TwosScore.java
@@ -6,13 +6,6 @@ public class TwosScore extends Score {
 	
 
 	public int calculateScore(int[] values) {
-/*		int twosSum = 0;
-		for (int i = 0; i < values.length; i++) {
-			if (values[i] == 2) {
-				twosSum++;
-			}
-		}
-		return 2*twosSum;*/
 		return countdice(values, 2);
 	}
 }
