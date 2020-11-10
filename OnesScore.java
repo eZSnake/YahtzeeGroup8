4088edd32a7ed9d59991982e1401d51cd35705b4
eZSnake <rehwalde@gmail.com>
2020-11-09 21:31:58 -0800

    Update OnesScore.java

--- OnesScore.java
+++ OnesScore.java
@@ -5,13 +5,6 @@ public class OnesScore extends Score {
 	}
 	
 	public int calculateScore(int[] values) {
-//		int onesSum = 0;
-//		for (int i = 0; i < values.length; i++) {
-//			if (values[i] == 1) {
-//				onesSum++;
-//			}
-//		}
-//		return onesSum;
 		return countdice(values, 1);
 	}
 }
