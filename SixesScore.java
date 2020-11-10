156e081ef83079ee41a4abc3a814d54f611c4b2f
eZSnake <rehwalde@gmail.com>
2020-11-09 21:32:12 -0800

    Update SixesScore.java

--- SixesScore.java
+++ SixesScore.java
@@ -5,13 +5,6 @@ public class SixesScore extends Score {
 	}
 	
 	public int calculateScore(int[] values) {
-//		int sixsSum = 0;
-//		for (int i = 0; i < values.length; i++) {
-//			if (values[i] == 6) {
-//				sixsSum++;
-//			}
-//		}
-//		return sixsSum;
 		return countdice(values, 6);
 	}
 }
